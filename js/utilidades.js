f7a66c1f615e73ca4b9e82d35b9f12af0832690d
JeanCarlosSM <125092297+JeanCarlosSM@users.noreply.github.com>
2023-05-12 23:06:46 -0500

    Add files via upload

---
+++ js/utilidades.js
@@ -0,0 +1,27 @@
+function createTagWithText(tag,text) {
+    var tagHtml = createTag(tag);
+    var textHtml = document.createTextNode(text);
+    tagHtml.appendChild(textHtml);
+    document.body.appendChild(tagHtml);
+    return tagHtml;
+}
+function addElementContainer(container, element) {
+    container.appendChild(element)
+}
+function addElement(element) {
+    document.body.appendChild(element);
+}
+function createTagImage(path) {
+    var tagImg = document.createElement("img");
+    tagImg.src = path
+    return tagImg
+}
+function createTagA(text, url) {
+    var tagImg = createTagWithText("a", text)
+    tagImg.href = url
+    return tagImg
+}
+function createTag(tag) {
+    var tagHtml = document.createElement(tag);
+    return tagHtml
+}
